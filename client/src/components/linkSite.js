1ceaab525922467868c34e76ad087c71b7ae8144
Samaresh Das <samareshmail679@gmail.com>
2023-03-28 01:03:31 +0530

    changed back to render

--- client/src/components/linkSite.js
+++ client/src/components/linkSite.js
@@ -1,4 +1,4 @@
-export const linkSite = "http://13.250.115.76:5000";
+export const linkSite = "https://dev-blog-p5s9.onrender.com";
 //http://localhost:5000
 //https://dev-blog-p5s9.onrender.com
 //http://13.250.115.76:5000
