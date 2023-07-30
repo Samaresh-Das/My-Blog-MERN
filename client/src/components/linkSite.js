f595b4698a1214881db47c19c46f347126da9526
Samaresh Das <samareshmail679@gmail.com>
2023-07-30 12:55:27 +0530

    shifted to cyclic backend due to aws cost

--- client/src/components/linkSite.js
+++ client/src/components/linkSite.js
@@ -1,4 +1,5 @@
-export const linkSite = "https://43.201.102.96";
+export const linkSite = "https://cute-cyan-dragonfly-boot.cyclic.app";
 //http://localhost:5000
 //https://dev-blog-p5s9.onrender.com
-//https://43.201.102.96/
+//https://43.201.102.96
+//https://cute-cyan-dragonfly-boot.cyclic.app
