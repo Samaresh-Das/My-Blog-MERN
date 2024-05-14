322c07ac21c632b6e6ad4d8fe2b4e6e66ad6ec37
Samaresh Das <samareshmail679@gmail.com>
2024-05-14 15:21:53 +0530

    changed backend server

--- client/src/components/linkSite.js
+++ client/src/components/linkSite.js
@@ -1,5 +1,6 @@
-export const linkSite = "https://cute-cyan-dragonfly-boot.cyclic.app";
+export const linkSite = "https://my-blog-mern.onrender.com";
 //http://localhost:8080
 //https://dev-blog-p5s9.onrender.com
 //https://43.201.102.96
 //https://cute-cyan-dragonfly-boot.cyclic.app
+//https://my-blog-mern.onrender.com
