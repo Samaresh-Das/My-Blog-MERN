d61b3ffd98a0e90e9b0e16754d943887a3550e5d
Samaresh Das <samareshmail679@gmail.com>
2024-09-24 15:25:24 +0530

    Moved to render backend

--- client/src/components/linkSite.js
+++ client/src/components/linkSite.js
@@ -1,4 +1,4 @@
-export const linkSite = "https://adaptable-radiance-production.up.railway.app";
+export const linkSite = "https://my-blog-mern.onrender.com";
 //https://adaptable-radiance-production.up.railway.app/
 //http://localhost:8080
 //https://my-blog-mern.onrender.com
